e32eee9792c757c5cd72da58170e3472ea0969b6
Brian Almada <brianalexisalmada84@gmail.com>
2023-04-21 23:35:10 -0300

    Importando e inicializando estado

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Product } from './../../models/product-model'
 
 @Component({
   selector: 'app-product',
@@ -7,7 +8,7 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductComponent {
 
-  @Input() title: string = '';
+  @Input() product!: Product;
 
 
 }
